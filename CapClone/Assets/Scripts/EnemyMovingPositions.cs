a370877709f100bbb5cf7a09e2c115b8372847b6
Philip Park <philippark0203@gmail.com>
2020-12-09 14:31:44 -0500

    Tight Spaces Level--My tight spaces level just has a bunch of moving enemies. I think it'll add a lot to careful planning and positioning!

---
+++ CapClone/Assets/Scripts/EnemyMovingPositions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovingPositions : MonoBehaviour
+{
+    public List<Transform> locations;
+    public Transform currentTarget;
+    public int locationNumber;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTarget = locations[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, currentTarget.position, Time.deltaTime * speed);
+        if (Vector3.Distance(this.transform.position, currentTarget.position) < .2f)
+        {
+            locationNumber++;
+            if (locationNumber == locations.Count)
+            {
+                locationNumber = 0;
+            }
+            currentTarget = locations[locationNumber];
+        }
+    }
+}
